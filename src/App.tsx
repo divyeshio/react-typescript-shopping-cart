37613d4a7de1f31028ce5884d6c8504ff2758317
Divyesh Bhandari <79130336+divyeshio@users.noreply.github.com>
2022-10-01 00:54:37 +0530

    Add shopping cart

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import { useState } from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css'
 import { Routes, Route } from "react-router-dom"
-import {Container} from "react-bootstrap"
-import { Home } from './pages/Home'
-import { Store } from './pages/Store'
-import { About } from './pages/About'
-import { NavBar } from './components/NavBar'
+import { Container } from "react-bootstrap"
+import { Home } from "./pages/Home"
+import { Store } from "./pages/Store"
+import { About } from "./pages/About"
+import { Navbar } from "./components/NavBar"
+import { ShoppingCartProvider } from "./context/ShoppingCartContext"
+
+
 
 function App() {
-
   return (
-    <>
-      <NavBar />
-      <Container className='mb-4'>
+    <ShoppingCartProvider>
+      <Navbar />
+      <Container className="mb-4">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/store" element={<Store />} />
-          <Route path="/about" element={<About /> } />
+          <Route path="/about" element={<About />} />
         </Routes>
       </Container>
-    </>
+    </ShoppingCartProvider>
   )
 }
 
